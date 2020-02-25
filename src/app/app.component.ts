cf2a2f286c8fd2c8e867e20f6ab79893fe150367
Miguel Jardinez <miguel_jardinez@hotmail.com>
2020-02-25 16:29:34 -0600

    Se añadio pipe de slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  nombre: string = 'Capitan America';
 
+  nombre = 'Capitan America';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 }

296b9ba2b29d35481aed4912ada773ce8f60bcff
Miguel Jardinez <miguel_jardinez@hotmail.com>
2020-02-26 09:59:54 -0600

    Se termina el proyecto donde se muestran todos los pipes de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { promise } from 'protractor';
+import { resolve } from 'dns';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,8 @@ export class AppComponent {
 
   nombre = 'Capitan America';
 
+  nombre2 = 'mIgUEl AnGeL JArDiNeZ rAmOS';
+
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   PI: number = Math.PI;
@@ -17,14 +21,30 @@ export class AppComponent {
 
   salario = 1234.5;
 
+  fecha: Date = new Date(); 
+
+  idioma: string = 'es-MX';
+
+  videoUrl: string = 'https://www.youtube.com/embed/1Nr_tqkMsJs?controls=0';
+
+  activar: boolean = true;
+
+  valorPromesa = new Promise<string> ((resolve) => {
+
+  setTimeout(() => {
+    resolve('Solicitud Procesada')
+  }, 4500);
+
+  });
+
+
   heroe = {
     nombre: 'Logan',
     clave: 'Wolverine',
     edad: 500,
     direccion: {
       calle: 'alegria',
-      numero: 519
+      casa: 519
     }
   }
-
 }
---
+++ src/app/pipes/capitalizado.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'capitalizado'
+})
+export class CapitalizadoPipe implements PipeTransform {
+
+  transform(value: string, todas: boolean = true): string {
+
+    value = value.toLowerCase();
+    let nombres = value.split(' ');
+    console.log(nombres);
+
+    if(todas){
+      nombres = nombres.map( nombre => {
+        return nombre[0].toUpperCase() + nombre.substr(1); 
+      });
+
+      console.log(nombres);
+     
+    } else {
+      nombres[0] = nombres[0][0].toUpperCase() + nombres[0].substr(1);
+    }
+
+    return nombres.join(' ');
+  }
+
+}

e2d193d0fbeca3a3cd7dd65ca830fa126e5db1af
Miguel Jardinez <miguel_jardinez@hotmail.com>
2020-02-25 20:46:31 -0600

    Se añadieron pipes de currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,22 @@ export class AppComponent {
 
   nombre = 'Capitan America';
 
-  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI: number = Math.PI;
+
+  porcentaje = 0.258;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'alegria',
+      numero: 519
+    }
+  }
 
 }
